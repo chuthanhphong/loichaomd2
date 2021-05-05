33ccd2dea98f30e655ee1c558173dc16f38a58f4
chuthanhphong <81786056+chuthanhphong@users.noreply.github.com>
2021-05-05 15:02:12 +0700

    add .

---
+++ loichao1/src/loichao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class loichao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhâp tên của bạn");
+        String name = sc.nextLine();
+        System.out.println("Hello "+ name);
+    }
+}
